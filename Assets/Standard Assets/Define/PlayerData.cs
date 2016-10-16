46ac2c507891617524bc278d061297119a3b0935
yinweli <yinweli@yahoo.com.tw>
2016-10-16 13:41:24 +0800

    新增存檔元件

--- Assets/Standard Assets/Define/PlayerData.cs
+++ Assets/Standard Assets/Define/PlayerData.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+using LibCSNStandard;
 
 public class PlayerData : MonoBehaviour
 {
@@ -8,24 +7,50 @@ public class PlayerData : MonoBehaviour
 
     public static int iTagCount = 0;
     public static string[] strTag = new string[50];
+
     // ------------------------------------------------------------------
-    void Start()
+    private void Start()
     {
         // 取得Tag總數.
         //iTagCount = PlayerPrefs.GetInt("iTagCount", 0);
         // 取得Tag名稱.
+
+        SaveData<TestOfSave> saveDatas = new SaveData<TestOfSave>();
+
+        saveDatas.Add(new TestOfSave() { GUID = new Argu(123), name = "都嚕嚕", note = "卡00巴00巴", money = 999, count = 1, });
+        saveDatas.Add(new TestOfSave() { GUID = new Argu("b"), name = "咖乃乃", note = "卡01巴10巴", money = 888, count = 3, });
+        saveDatas.Add(new TestOfSave() { GUID = new Argu(223), name = "打逼逼", note = "卡11巴11巴", money = 777, count = 5, });
+        saveDatas.Add(new TestOfSave() { GUID = new Argu("d"), name = "得嚕啦", note = "卡21巴12巴", money = 666, count = 7, });
+        saveDatas.Add(new TestOfSave() { GUID = new Argu("e"), name = "軟七基", note = "卡34巴43巴", money = 123456789, count = 9, });
+
+        string json = saveDatas.ToString();
+
+        Debug.Log("json=" + json);
+
+        saveDatas.Load(json);
+
+        SaveItor<TestOfSave> itor = saveDatas.GetItor();
+
+        while(itor.IsEnd() == false)
+        {
+            Debug.Log("data=" + itor.Data().ToString());
+            itor.Next();
+        }
     }
+
     // ------------------------------------------------------------------
     public static float GetMoney()
     {
         return fMoney;
     }
+
     // ------------------------------------------------------------------
     public static bool AddMoney(float fValue)
     {
         fMoney = fMoney + fValue;
         return true;
     }
+
     // ------------------------------------------------------------------
     public static bool AddTag(string sValue)
     {
@@ -33,5 +58,18 @@ public class PlayerData : MonoBehaviour
         iTagCount++;
         //PlayerPrefs.SetString(, );
         return true;
+    }
+
+    public class TestOfSave : Save
+    {
+        public string name = "";
+        public string note = "";
+        public int money = 0;
+        public int count = 0;
+
+        public override string ToString()
+        {
+            return "name=" + name + ", note=" + note + ", money=" + money + ", count=" + count;
+        }
     }
 }
---
+++ Assets/Standard Assets/Libcsnstandard/save/save.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibCSNStandard
+{
+    /// <summary>
+    /// save類別
+    /// </summary>
+    public class Save
+    {
+        /// <summary>
+        /// 索引字串
+        /// </summary>
+        public string GUID = "";
+    }
+
+    /// <summary>
+    /// save資料類別
+    /// </summary>
+    public class SaveData<T> where T : Save
+    {
+        /// <summary>
+        /// save資料列表 <索引字串, save物件>
+        /// </summary>
+        private Dictionary<string, T> datas = new Dictionary<string, T>();
+
+        /// <summary>
+        /// 讀取資料
+        /// </summary>
+        /// <param name="json">資料字串</param>
+        /// <returns>true表示成功, false則否</returns>
+        public bool Load(string json)
+        {
+            datas.Clear();
+
+            foreach (string itor in json.Split(new char[] { '\n' }))
+            {
+                T data = Json.ToObject<T>(itor);
+
+                if (data != null)
+                    datas.Add(data.GUID, data);
+            }//for
+
+            return true;
+        }
+
+        /// <summary>
+        /// 新增資料
+        /// </summary>
+        /// <param name="data">資料物件</param>
+        public void Add(T data)
+        {
+            datas[data.GUID] = data;
+        }
+
+        /// <summary>
+        /// 移除資料
+        /// </summary>
+        /// <param name="guid">索引物件</param>
+        public void Remove(Argu guid)
+        {
+            datas.Remove(guid);
+        }
+
+        /// <summary>
+        /// 取得資料物件
+        /// </summary>
+        /// <param name="guid">索引物件</param>
+        /// <returns>資料物件</returns>
+        public Save Get(Argu guid)
+        {
+            return datas.ContainsKey(guid) ? datas[guid] : null;
+        }
+
+        /// <summary>
+        /// 取得save列舉物件
+        /// </summary>
+        /// <returns>save列舉物件</returns>
+        public SaveItor<T> GetItor()
+        {
+            return new SaveItor<T>(this);
+        }
+
+        /// <summary>
+        /// 取得資料數量
+        /// </summary>
+        /// <returns>資料數量</returns>
+        public int Count()
+        {
+            return datas.Count;
+        }
+
+        /// <summary>
+        /// 取得索引列表
+        /// </summary>
+        /// <returns>索引列表</returns>
+        public List<string> List()
+        {
+            return new List<string>(datas.Keys);
+        }
+
+        /// <summary>
+        /// 將資料轉換為字串
+        /// </summary>
+        /// <returns>資料字串</returns>
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            datas.Values.ToList().ForEach(itor => stringBuilder.Append(Json.ToString(itor) + "\n"));
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    /// <summary>
+    /// save列舉類別
+    /// </summary>
+    public class SaveItor<T> where T : Save
+    {
+        /// <summary>
+        /// 資料物件
+        /// </summary>
+        private SaveData<T> data = null;
+
+        /// <summary>
+        /// 索引列表
+        /// </summary>
+        private List<string> list = null;
+
+        /// <summary>
+        /// 目前位置
+        /// </summary>
+        private int pos = 0;
+
+        public SaveItor(SaveData<T> data)
+        {
+            if (data == null)
+                return;
+
+            this.data = data;
+            this.list = data.List();
+            this.pos = 0;
+        }
+
+        private bool Check()
+        {
+            return data != null && list != null;
+        }
+
+        /// <summary>
+        /// 取得save
+        /// </summary>
+        /// <returns>save物件</returns>
+        public Save Data()
+        {
+            return Check() && pos < list.Count ? data.Get(list[pos]) : null;
+        }
+
+        /// <summary>
+        /// 移動到下一個
+        /// </summary>
+        public void Next()
+        {
+            if (Check() && pos < list.Count)
+                ++pos;
+        }
+
+        /// <summary>
+        /// 取得否到結尾
+        /// </summary>
+        /// <returns>true表示到結尾, false則否</returns>
+        public bool IsEnd()
+        {
+            return Check() == false || pos >= list.Count;
+        }
+    }
+}
